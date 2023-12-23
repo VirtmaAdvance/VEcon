2d4795746abafb4277223610d346fbd4f83f6509
Daniel K. Valente <dan.k.valente@gmail.com>
2023-12-22 17:38:52 -1000

    - Removed whitespace.

--- TaxRate.cs
+++ TaxRate.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VEconomy
+﻿namespace VEconomy
 {
-	public class TaxRate : VCollection
+	public class TaxRate : VCollection<TaxRateItem>
 	{
 
 
