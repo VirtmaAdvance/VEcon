2337afd3a3e15625bb26dee1c76a890e15490e90
Daniel K. Valente <dan.k.valente@gmail.com>
2023-12-22 14:30:11 -1000

    - Added indexing for class collection.

--- CurrencyWorth.cs
+++ CurrencyWorth.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections;
-
-namespace VEconomy
+﻿namespace VEconomy
 {
 	/// <summary>
 	/// Stores a collection of currencies and their relative global worth scale.
 	/// </summary>
 	public class CurrencyWorth : VCollection<CurrencyWorthItem>
 	{
+
+		/// <summary>
+		/// Gets or sets the item at the given key/index.
+		/// </summary>
+		/// <param name="key"></param>
+		/// <returns></returns>
+		public CurrencyWorthItem this[string key]
+		{
+			get=>this[IndexOf(key)];
+			set=>this[IndexOf(key)]=value;
+		}
 
 
 		private void Update(string name, CurrencyWorthItem item) => Update(IndexOf(name), item);
@@ -16,7 +25,7 @@ namespace VEconomy
 			if(IsIndexValid(index))
 				Items[index]=item;
 		}
-
+		/// <inheritdoc cref="VCollection.IndexOf(T)"/>
 		public int IndexOf(string name)
 		{
 			for(int i=0;i<Length;i++)
